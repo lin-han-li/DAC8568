58e552fd8f2852f4a9579142266c575f1c243b5b
lin-han-li <1526595636@qq.com>
2026-07-01 23:01:02 +0800

    fix: keep dac spi gpio slew conservative

--- Core/Src/spi.c
+++ Core/Src/spi.c
@@ -159,7 +159,7 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* spiHandle)
     GPIO_InitStruct.Pin = DAC8568_DIN_Pin|DAC85_CLK_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
